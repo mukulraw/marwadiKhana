5fe5d1aed42c1c60c34969d444a36e546bee5035
mukul <mukulraw199517@gmail.com>
2017-01-29 00:08:50 -0800

    started

---
+++ app/src/main/java/com/mrtechs/apps/mk/GetStarted.java
@@ -0,0 +1,75 @@
+package com.mrtechs.apps.mk;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import me.relex.circleindicator.CircleIndicator;
+
+public class GetStarted extends AppCompatActivity {
+
+    ViewPager pager;
+    Button getStarted;
+    CircleIndicator indicator;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_started);
+
+        pager = (ViewPager) findViewById(R.id.pager);
+        getStarted = (Button) findViewById(R.id.get_started);
+        indicator = (CircleIndicator) findViewById(R.id.indicator);
+
+
+        PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager());
+
+        pager.setAdapter(adapter);
+        indicator.setViewPager(pager);
+
+
+
+
+
+    }
+
+
+    private class PagerAdapter extends FragmentStatePagerAdapter{
+
+        PagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return new pages();
+        }
+
+        @Override
+        public int getCount() {
+            return 4;
+        }
+    }
+
+
+    public static class pages extends Fragment{
+
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+
+            return inflater.inflate(R.layout.pager_images , container , false);
+        }
+    }
+
+
+}
--- app/src/main/java/com/mrtechs/apps/mk/Splash.java
+++ app/src/main/java/com/mrtechs/apps/mk/Splash.java
@@ -1,13 +1,35 @@
 package com.mrtechs.apps.mk;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Splash extends AppCompatActivity {
+
+    Timer t;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        t = new Timer();
+
+        t.schedule(new TimerTask() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(Splash.this , GetStarted.class);
+                startActivity(intent);
+                finish();
+
+
+
+            }
+        } , 1500);
+
     }
 }
